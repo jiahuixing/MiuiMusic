1423f5fac93ebcfe8fe8954032eb2aedc1f8cfe9
jiahuixing <jiahuixing@gmail.com>
2014-02-27 17:49:16 +0800

    Initial commit

---
+++ src/com/miui/player/MusicVip.java
@@ -0,0 +1,16 @@
+package com.miui.player;
+
+
+/**
+ * Project name : MiuiMusic
+ * Package name : com.miui.player
+ * Created by jiahuixing
+ * Created on 2014-02-25
+ */
+public class MusicVip {
+
+    private static int TYPE;
+
+
+
+}
