2a5fdc89eccc2df248ca54c2ed50738035cd7169
jiahuixing <jiahuixing@gmail.com>
2014-03-03 11:04:31 +0800

    json test

--- src/com/miui/player/MusicVip.java
+++ src/com/miui/player/MusicVip.java
@@ -7,12 +7,21 @@ package com.miui.player;
  * Created on 2014-02-25
  */
 
-import net.sf.json.JSONObject;
+import net.sf.json.JSONArray;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class MusicVip {
 
-    private static int TYPE;
+    public static void main(String[] args) {
 
-    JSONObject n = JSONObject.fromObject();
+        List<String> list = new ArrayList<String>();
+        list.add("first");
+        list.add("second");
+        JSONArray json = JSONArray.fromObject(list);
+        System.out.printf("json=%s", json);
+    }
+
 
 }
